fff3a9819cccdc9e92b4e140496726a086dfee63
Sukshan <sukshan.jadhao@hbwsl.com>
2024-02-27 12:53:05 +0530

    added controller for the example module

---
+++ app/code/Training/Example/Controller/Index/index.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace Training\Example\Controller\Index;
+
+
+use Magento\Framework\App\ActionInterface;
+
+class Index implements ActionInterface{
+    public function execute(){
+        die('Example');
+    }
+}
