e1ed85ef5a35447e82c2d4e258d70e4bb57431c8
sukshan <sukshan.jadhao@hbwsl.com>
2024-02-29 11:56:56 +0530

    added head script

---
+++ app/code/Training/HeaderFooterScripts/Controller/Index/Index.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace Training\HeaderFooterScripts\Controller\Index;
+
+
+use Magento\Framework\App\ActionInterface;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index implements ActionInterface{
+    protected $pageFactory;
+    public function __construct(PageFactory $pageFactory){
+        $this->pageFactory = $pageFactory;
+    }
+    public function execute(){
+        return $this->pageFactory->create();
+    }
+}
--- app/code/Training/HeaderFooterScripts/registration.php
+++ app/code/Training/HeaderFooterScripts/registration.php
@@ -6,6 +6,6 @@ use \Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(
     ComponentRegistrar::MODULE,
-    'Training_LayoutExample',
+    'Training_HeaderFooterScripts',
     __DIR__
 );
